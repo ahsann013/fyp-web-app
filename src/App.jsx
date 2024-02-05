69dbd4675c01400d21ed3afa3a4f851acf5368a3
ahsann013 <ahsan.yousaf1828@gmail.com>
2024-02-06 03:02:10 +0500

    fix routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import { useState } from 'react'
 
 import './App.css'
-import { Route,Routes } from 'react-router'
+import { Route,Routes } from 'react-router-dom'
 
 
 import Navbar from './components/Navbar'
 import Login from './components/Login'
 import Info from './components/Info'
-import About from './components/About'
+import About from './pages/About'
 import Signup from './components/Signup'
 import DashBoard from './components/DashBoard'
 
@@ -19,11 +19,11 @@ function App() {
   
      <Navbar/>
       <Routes>
-        <Route path='/' />
-        <Route path='/info' element={<Info/>} />
-        <Route path='/about' element={<About />} />
-        <Route path='/signup' element={<Signup />} />
-        <Route path='/dashboard' element={<DashBoard/>} />
+        <Route path='/fyp-web-app' />
+        <Route path='/fyp-web-app/info' element={<Info/>} />
+        <Route path='/fyp-web-app/about' element={<About />} />
+        <Route path='/fyp-web-app/signup' element={<Signup />} />
+        <Route path='/fyp-web-app/dashboard' element={<DashBoard/>} />
       </Routes>
     
       
